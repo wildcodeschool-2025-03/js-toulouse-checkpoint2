1fb13697eb344e029ce5bea6d133abf933d9abd3
ArthurHtbk <arthur.heurtebise@gmail.com>
2025-05-19 15:41:26 +0200

    algo 1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -11,7 +11,15 @@ Exemples :
 
 function countLetters(givenString: string, letter: string): number {
   // Ton code ici !
-  return 0;
+  let count = 0;
+
+  for (let i = 0; i < givenString.length; i++) {
+    if (givenString[i] === letter) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 export default countLetters;

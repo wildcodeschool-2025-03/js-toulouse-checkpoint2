8713d783b356edbf27d907cde497e12035f76cc0
Covarel Camille <covarelcamille@gmail.com>
2025-05-19 09:56:41 +0200

    First Algo

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,7 +11,6 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
-  // Ton code ici !
   return [0, 1];
 }
 

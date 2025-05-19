b11b3326e56fcbf41467205591d8c8251b9e1d71
zarzour <ridouane@MacBook-Air-de-ridouane.local>
2025-05-19 10:45:39 +0200

    ALGO1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,14 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+  let count = 0;
+  for (let i = 0; i < givenString.length; i++) {
+    if (givenString[i].toLowerCase() === letter.toLowerCase()) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 export default countLetters;
